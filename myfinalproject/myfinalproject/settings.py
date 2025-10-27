0fca720fef78228be641d1ef08d1843d131df50f
shanhasanova120-ops <shanhasanova120@gmail.com>
2025-10-27 13:50:36 +0400

    Update settings.py

--- myfinalproject/myfinalproject/settings.py
+++ myfinalproject/myfinalproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls'
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+

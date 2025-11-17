012d657b0059ecb997b69b78ce1b48efe910a09e
shanhasanova120-ops <shanhasanova120@gmail.com>
2025-11-17 15:07:48 +0400

    Update views.py

--- myfinalproject/polls/views.py
+++ myfinalproject/polls/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Question
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
